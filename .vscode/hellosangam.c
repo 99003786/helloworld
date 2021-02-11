c543476aec43096023a0a53f53711f4023e62897
Souvik Jana <78857077+99003784@users.noreply.github.com>
2021-02-11 11:40:24 +0530

    adding an extra statement

--- .vscode/hellosangam.c
+++ .vscode/hellosangam.c
@@ -5,5 +5,6 @@ int main()
     printf("Please Say Hi or Hello\n");
     scanf("%s",&a);
 printf("Hello World This Is Sangam Patel\n");
+  printf("Hello World My name is Souvik Jana\n");
     return 0;
 }
